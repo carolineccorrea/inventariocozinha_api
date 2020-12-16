94e34c1c34c1bf9470348dc9bd3529982dbe1f21
Carol <castrocaroline11@gmail.com>
2020-12-16 07:38:08 -0300

    add changes

--- startUp.ts
+++ startUp.ts
@@ -1,21 +1,28 @@
 import * as express from 'express'
 import Db from './infra/db';
+import * as bodyParser from 'body-parser';
 
 class startUp {
     public app: express.Application;
     private _db: Db;
+    private bodyParser;
 
     constructor() {
         this.app = express();
         this.routes();
-
+        this.middler();
         this._db = new Db;
         this._db.createConnection();
     }
 
-    routes(){
-        this.app.route("/").get((req,res) => {
-            res.send({ teste:'ok' })
+    middler() {
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    routes() {
+        this.app.route("/").get((req, res) => {
+            res.send({ teste: 'ok' })
         })
     }
 }
